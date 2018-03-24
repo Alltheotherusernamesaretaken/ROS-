939e8089fadf1d1725b737a090a3586e536ea94e
Unknown <mts341@msstate.edu>
2018-03-24 18:43:20 -0500

    implemented basic class methods

--- lib/PID_Controller/src/PID_Controller_ABC.cpp
+++ lib/PID_Controller/src/PID_Controller_ABC.cpp
@@ -37,3 +37,98 @@ void inline PIDControllerABC::unlock()
 {
   xSemaphoreGive( mutex );
 }
+
+virtual int PIDControllerABC::update(){
+  lock();
+  for (i=0, i<8, i++){
+
+  }
+  unlock();
+}
+
+int PIDControllerABC::set_PID_gains(int i, double kp, double ki, double kd){
+  lock();
+  motPID[i]->setTunings(kp, ki, kd);
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::get_PID_gains(int i, double *kp, double *ki, double *kd){
+  lock();
+  kp = motPID[i]->GetKp();
+  ki = motPID[i]->GetKi();
+  kd = motPID[i]->GetKd();
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::set_PID_setpoint(int i,double setp){
+  lock();
+  motSetpoint[i] = setp;
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::get_PID_setpoint(int i,double *setp){
+  lock();
+  setp = motSetpoint[i];
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::set_sensor_gain(int i,double gain){
+  lock();
+  motSensorGain[i] = gain;
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::get_sensor_gain(int i,double *gain){
+  lock();
+  gain = motSensorGain[i];
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::set_PID_control_type(uint8_t ctype){
+  lock();
+  motControlType[i] = ctype;
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::get_PID_control_type(uint8_t *ctype){
+  lock();
+  ctype = motControlType[i];
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::set_PID_proportion_type(uint8_t ptype){
+  lock();
+  motProportionType[i] = ptype;
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::get_PID_proportion_type(uint8_t *ptype){
+  lock();
+  ptype = motProportionType[i];
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::zero_PID_sensor(int i){
+  lock();
+  motSensorVal[i] = 0;
+  unlock();
+  return 0;
+}
+
+int PIDControllerABC::_write_PWM_values(){
+  lock();
+  for(int i=0, i<8, i++){
+    motPWMChannel[i] = motPWM[i];
+    }
+  unlock();
+}
