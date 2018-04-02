9dc695e1c523033aaad847bdfce83546b0262c0b
Lucas D. Cagle <ldc290@msstate.edu>
2018-04-02 18:41:47 -0500

    Added adc driver

--- lib/motor_sensor_drivers/motor_ADS1015_driver.h
+++ lib/motor_sensor_drivers/motor_ADS1015_driver.h
@@ -3,30 +3,17 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_ADS1015.h>
+#include "../Adafruit_ADS1X15/Adafruit_ADS1015.h"
 #include "motor_sensor_driver_ABC.h"
 
 class MotorADS1015Driver :  public MotorSensorDriverABC
 {
 public:
 
-  MotorADS1015Driver(int, int*);
+  MotorADS1015Driver();
+  MotorADS1015Driver(int);
 
   int update();
-
-  int get_angular_position(int, double*);
-  int get_angular_positions(int*, double**);
-
-  int get_angular_velocity(int, double*);
-  int get_angular_velocities(int*, double**);
-
-  int reset_position(int, double);
-
-  int get_angular_gain(int, double*);
-  int set_angular_gain(int, double);
-
-  int get_angular_bias(int, double*);
-  int set_angular_bias(int, double);
 
 private:
   Adafruit_ADS1015 ads;
