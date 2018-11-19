2108b8317d90a37a66b1cfa7d3a8ec68de8e9214
Lucas D. Cagle <daftpwner@gmail.com>
2018-11-19 16:05:17 -0600

    Pruned interface and added default methods: #1
    Removed methods for multi-motor commands. Additionally, default methods were implemented for basic methods.

--- lib/MotorInterfaceABC/MotorInterfaceABC.h
+++ lib/MotorInterfaceABC/MotorInterfaceABC.h
@@ -1,9 +1,8 @@
-
+#include <Arduino.h>
 class MotorInterfaceABC
 {
 private:
     bool pos_ctrl=false;
-    int num_motors=1;
     double pos_set=0;
     double vel_set=0;
     double pos_upper_limit=-1e7;
@@ -21,77 +20,86 @@ public:
     ///
     /// \param setpoint The new setpoint value
     /// \param idx Optional channel index
-    virtual int setPos(double setpoint, int idx=0);
+    int setPos(double setpoint, int idx=0){
+        pos_set = setpoint;
+        return 0;
+    }
+
     /// \brief Sets the new Velocity setpoint
     ///
     /// \param setpoint The new setpoint value
     /// \param idx Optional channel index
-    virtual int setVel(double setpoint, int idx=0);
-    /// \brief Sets the new Position setpoints for multiple motors
-    ///
-    /// \param setpoints The array of new setpoints
-    /// \param indexes The array of indexes
-    /// \param idx Optional channel index
-    virtual int setPos(double* setpoints, int* indexes, int idx=0);
-    /// \brief Sets the new Velocity setpoints for multiple motors
-    ///
-    /// \param setpoints The array of new setpoints
-    /// \param indexes The array of indexes
-    /// \param idx Optional channel index
-    virtual int setVel(double* setpoints, int* indexes, int idx=0);
+    int setVel(double setpoint, int idx=0){
+        vel_set = setpoint;
+        return 0;
+    }
+
     /// \brief Gets Position setpoint
     ///
     /// \param setpoints Pointer to hold setpoint
     /// \param idx Optional channel index
     virtual int getPos(double* setpoint, int idx=0);
+
     /// \brief Gets Velocity setpoint
     ///
     /// \param setpoints Pointer to hold setpoint
     /// \param idx Optional channel index
     virtual int getVel(double* setpoint, int idx=0);
-    /// \brief Gets the Position setpoints for multiple motors
-    ///
-    /// \param setpoints The array to hold setpoints
-    /// \param indexes The array of indexes
-    /// \param idx Optional channel index
-    virtual int getPos(double* setpoints, int* indexes, int=0);
-    /// \brief Gets the Velocity setpoints for multiple motors
-    ///
-    /// \param setpoints The array to hold setpoints
-    /// \param indexes The array of indexes
-    /// \param idx Optional channel index
-    virtual int getVel(double* setpoints, int* indexes, int=0);
+
     /// \brief Sets the Position limits
     ///
     /// \param lower Lower limit
     /// \param upper Upper limit
     /// \param idx Optional channel index
-    virtual int setPosLimits(double lower=-1e7, double=1e7, int=0);
+    int setPosLimits(double lower=-1e7, double upper=1e7, int idx=0){
+        if (lower>upper) return 1;
+        if (lower>-1e7) pos_lower_limit = lower;
+        if (upper<1e7) pos_upper_limit = upper;
+    }
+
     /// \brief Sets the Velocity limits
     ///
     /// \param lower Lower limit
     /// \param upper Upper limit
     /// \param idx Optional channel index
-    virtual int setVelLimits(double lower=-1e7, double=1e7, int=0);
+    int setVelLimits(double lower=-1e7, double upper=1e7, int idx=0){
+        if (lower>upper) return 1;
+        if (lower>-1e7) vel_lower_limit = lower;
+        if (upper<1e7) vel_upper_limit = upper;
+    }
+
     /// \brief Gets the Position limits
     ///
     /// \param lower Lower limit pointer
     /// \param upper Upper limit pointer
     /// \param idx Optional channel index
-    virtual int getPosLimits( double* lower, double* upper, int=0);
+    int getPosLimits( double* lower, double* upper, int idx=0){
+        if (lower!=NULL) *lower = pos_lower_limit;
+        if (upper!=NULL) *upper = pos_upper_limit;
+    }
+
     /// \brief Gets the Velocity limits
     ///
     /// \param lower Lower limit pointer
     /// \param upper Upper limit pointer
     /// \param idx Optional channel index
-    virtual int getVelLimits( double* lower, double* upper, int=0);
+    int getVelLimits( double* lower, double* upper, int idx=0){
+        if (lower!=NULL) *lower = vel_lower_limit;
+        if (upper!=NULL) *upper = vel_upper_limit;
+    }
+
     /// \brief Sets the control type (Pos/Vel)
     ///
     /// \param pos_ctrl Control type (True/False) (Pos/Vel)
-    virtual int setPosCtrl(bool, int=0);
+    int setPosCtrl(bool posctrl, int=0){
+        pos_ctrl = posctrl;
+    }
+    
     /// \brief Gets the control type (Pos/Vel)
     ///
     /// \param pos_ctrl Pointer for control type (True/False) (Pos/Vel)
-    virtual int getPosCtrl(bool*, int=0);
+    int getPosCtrl(bool* posctrl, int=0){
+        *posctrl = pos_ctrl;
+    }
 
 };
